f4f8b978389b3efbe19488ea1ee55b963c830e9d
Abdurahmon <ibrohimovabdurahmon68@gmail.com>
2025-07-18 18:24:09 +0500

    some

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -32,16 +32,16 @@ const Hero = () => {
         <Image
           src={"/hero-ceo.png"}
           alt="Business Diplomat"
-          width={800}
-          height={800}
+          width={1000}
+          height={1000}
           className="mx-auto object-cover 2xl:w-[900px] 2xl:h-[900px] lg:w-[580px] lg:h-[580px] md:w-[1000px] md:h-[1000px] w-[600px] h-[600px]"
         />
       </div>
-      <p className="transform scale-y-150 absolute 2xl:top-173 lg:top-110 md:top-220 top-150 inset-x-0 flex items-center justify-center z-50 bg-gradient-to-r from-[#707070] via-[#FFFFFF] to-[#707070] bg-clip-text text-transparent 2xl:text-5xl lg:text-4xl md:text-4xl text-[20px] font-semibold uppercase">
+      <p className="transform scale-y-150 absolute 2xl:top-173 lg:top-110 md:top-220 top-140 inset-x-0 flex items-center justify-center z-50 bg-gradient-to-r from-[#707070] via-[#FFFFFF] to-[#707070] bg-clip-text text-transparent 2xl:text-5xl lg:text-4xl md:text-4xl text-[20px] font-semibold uppercase">
         Xalqaro biznes ta&apos;lim loyihasi
       </p>
 
-      <Button variant="custom" size="custom" className="absolute z-50 2xl:top-195 lg:top-125 md:top-240 top-162 left-1/2 -translate-x-1/2">
+      <Button variant="custom" size="custom" className="absolute z-50 2xl:top-195 lg:top-125 md:top-240 top-152 left-1/2 -translate-x-1/2">
         Loyihada qatnashish uchun ro’yxatdan o’ting
       </Button>
 
